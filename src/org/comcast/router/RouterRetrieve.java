9671b9a0f96974b64e320a0eb29644947cb5568d
Quality of Service <Quality of Service@192.168.1.104>
2012-11-30 21:28:24 -0300

    14º Commit: Agregados los Routers que faltan (Input y Retrieve)

---
+++ src/org/comcast/router/RouterRetrieve.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.comcast.router;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ *
+ * @author Quality of Service
+ */
+public class RouterRetrieve implements Job{
+
+    @Override
+    public void execute(JobExecutionContext jec) throws JobExecutionException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}

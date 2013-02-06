38e064f9fa0659dd3b741fb994dee9be34cf96bc
Signalrunner <Signalrunner@Droid>
2013-02-06 15:10:53 -0300

    59º: Agregado Javadoc.

---
+++ src/org/comcast/router/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Posee todas las clases que dan soporte a las tareas programadas y el listado
+ * de los archivos tanto remotos como locales. <br> Implementacion del patron
+ * Router.
+ */
+package org.comcast.router;
